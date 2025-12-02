fcfaeeb5251b3a7cf4fe4443af4c4cccc02d89d6
uncle-dev <jhonbrian@uncle-dev.com>
2025-12-02 13:52:38 +0800

    update: component

--- components/Header.tsx
+++ components/Header.tsx
@@ -116,7 +116,6 @@ export default function Header() {
                 }`}
               >
                 {item.label}
-                hello
               </Link>
             );
           })}
